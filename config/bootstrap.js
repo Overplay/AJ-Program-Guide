bfab4e2e7ebd34b207441f0b2b58eca9d8f7b4cd
Ryan Hartzell <ryanhartzell115@gmail.com>
2016-10-05 14:16:16 -0700

    Cronjob to update listings

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -9,9 +9,32 @@
  * http://sailsjs.org/#!/documentation/reference/sails.config/sails.config.bootstrap.html
  */
 
+var CronJob = require('cron').CronJob;
+var request = require('superagent-bluebird-promise');
+
 module.exports.bootstrap = function(cb) {
 
   // It's very important to trigger this callback method when you are finished
   // with the bootstrap!  (otherwise your server will never lift, since it's waiting on the bootstrap)
+
+  var job = new CronJob('0 0 0 * * *', function () {
+    Lineup.find({})
+      .then( function (lineups) {
+        _.forEach(lineups, function (lineup) {
+          request
+            .get(sails.tvmedia.url + '/lineups/' + lineup.lineupID + "/listings")
+            .query({ lineupID: lineup.lineupID , api_key: sails.tvmedia.api_key })
+            .then( function (res) {
+              lineup.listings = res.data;
+              lineup.save();
+            })
+        })
+      })
+  }, function () {
+    sails.log("Lineups updated")
+  }, true);
+
+
+
   cb();
 };
