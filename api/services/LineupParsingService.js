f0b93b675a32e74c4783373824eec44bda399e42
Ryan Hartzell <ryanhartzell115@gmail.com>
2016-10-13 14:41:11 -0700

    Listings parsing

--- api/services/LineupParsingService.js
+++ api/services/LineupParsingService.js
@@ -5,19 +5,40 @@ module.exports = {
 
   parse: function(listings, lineupID) {
     //TODO parse a lineup by programs and save the programs
+    sails.log.info("Parsing lineup " + lineupID);
 
     async.each(listings, function (program) {
-      Program.create({
+      Program.findOne({
         programID: program.showID,
         programName: program.showName,
         channel: program.channelNumber,
-        startTime: new Date(program.listDateTime),
-        duration: program.duration,
-        description: program.description,
-        carrier: program.network,
-        extra: program,
-        lineup: lineupID
+        carrier: program.network
       })
+        .then( function (p) {
+          if (p) {
+            sails.log.verbose(program.showName + " already exists in database for lineup " + lineupID);
+          }
+          else {
+            Program.create({
+              programID: program.showID,
+              programName: program.showName,
+              channel: program.channelNumber,
+              startTime: new Date(program.listDateTime),
+              duration: program.duration,
+              description: program.description,
+              carrier: program.network,
+              extra: program,
+              lineup: lineupID
+            })
+              .then( function () {
+                sails.log.verbose(program.showName + " has been initialized");
+              })
+              .catch( function (err) {
+                sails.log.error(err);
+              })
+          }
+        })
+
     });
 
 
