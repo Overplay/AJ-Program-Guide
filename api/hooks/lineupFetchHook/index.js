ee45cf07e219216d5aac6c5c6f7c7fdfe3f092d9
Ryan Hartzell <ryanhartzell115@gmail.com>
2016-11-16 15:25:29 -0800

    More lineup parsing improvements

--- api/hooks/lineupFetchHook/index.js
+++ api/hooks/lineupFetchHook/index.js
@@ -42,35 +42,15 @@ module.exports = function lineupFetchHook(sails) {
 
       sails.log.info('Begin updating lineups');
 
+
       Lineup.find({active: true})
         .then(function (lineups) {
-          /*
-           var chain = Promise.resolve();
-           _.forEach(lineups, function (lineup) {
-           var last = moment(lineup.lastAccessed);
-           if (last.diff(moment(), 'days') > 7) {
-           lineup.active = false;
-           chain = chain.then( function () { return lineup.save() });
-           }
-           else {
-           sails.log.debug('Accessing api');
-           var endTime = moment().add(14, 'days').subtract(1, 'millisecond').toISOString();
 
-           chain = chain.then(function () {
-           return request
-           .get(sails.config.tvmedia.url + '/lineups/' + lineup.lineupID + "/listings")
-           .query({lineupID: lineup.lineupID, api_key: sails.config.tvmedia.api_key, end: endTime})
-           .then(function (res) {
-           LineupParsingService.parse(res.body, lineup.id);
-           return lineup.save();
-           })
-           })
-           }
-           });*/
           async.eachSeries(lineups, function (lineup, cb) {
             //save the lineup THEN retrieve and parse listings
             //sails.log.debug(lineup)
 
+            var startUpdate = moment();
             var endTime = moment().add(14, 'days').subtract(1, 'millisecond').toISOString();
 
             request
@@ -81,7 +61,7 @@ module.exports = function lineupFetchHook(sails) {
                 return LineupParsingService.parse(res.body, lineup.lineupID);
               })
               .then(function(){
-                sails.log.debug("callback");
+                sails.log.debug("Lineup " + lineup.lineupID + " updated in " + moment().diff(startUpdate, 'minutes') + " minutes");
                 return cb();
               })
               .catch(function(err){
@@ -90,9 +70,9 @@ module.exports = function lineupFetchHook(sails) {
               })
           });
 
-          setTimeout(sails.hooks.lineupfetchhook.fetch, cronDelay);
-
       })
+
+      setTimeout(sails.hooks.lineupfetchhook.fetch, cronDelay);
     }
   }
 };
